8186d4f200b6dbf511aeee10349474329c6a6288
Martin Hasoň <martin.hason@gmail.com>
2018-06-20 11:49:25 +0200

    [base] Add support for copying /mnt/rootfs to /

---
+++ base/rootfs/docker-entrypoint.d/05-rootfs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Copy rootfs
+#
+# (c) 2018 Martin Hasoň <martin.hason@gmail.com>
+#
+
+set -e
+
+if [ -d /mnt/rootfs ]; then
+    if [ ! -w / ]; then
+        printf "\033[30;41mUnable to copy \"/mnt/rootfs\" to \"/\" (permission denied)\033[0m\n" >&2
+        return
+    fi
+
+    cp -rpf /mnt/rootfs/. /
+fi
