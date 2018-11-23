44bb04160130521d948bf5ce372c401254277b1b
Martin Hasoň <martin.hason@gmail.com>
2018-11-24 00:52:40 +0100

    [base] Rewrite user handling

--- base/rootfs/docker-entrypoint.d/10-user.sh
+++ base/rootfs/docker-entrypoint.d/10-user.sh
@@ -2,103 +2,102 @@
 #
 # Create user and group
 #
-# (c) 2016 Martin Hasoň <martin.hason@gmail.com>
+# (c) 2016, 2018 Martin Hasoň <martin.hason@gmail.com>
+#
+# https://github.com/Graham42/mapped-uid-docker
+# https://github.com/reduardo7/docker-host-user
 
 set -e
 
-if [ x"$SUEXEC" != x ]; then
-    SUEXEC_USER_ID=$(echo "$SUEXEC" | awk -F: '{print $1}')
-    SUEXEC_GROUP_ID=$(echo "$SUEXEC" | awk -F: '{print $2}')
-    SUEXEC_USER_NAME=$(echo "$SUEXEC" | awk -F: '{print $3}')
-    SUEXEC_UMASK=$(echo "$SUEXEC" | awk -F: '{print $4}')
-fi
+non_empty()(
+    for value in "$@"; do
+        if [ -n "$value" ]; then
+            printf '%s' "$value"
+            return
+        fi
+    done
+)
 
-export USER_ID=${USER_ID:-${SUEXEC_USER_ID:-$(id -u)}}
-export USER_NAME=${USER_NAME:-${SUEXEC_USER_NAME}}
-export USER_MASK=${USER_MASK:-${SUEXEC_UMASK:-$(umask)}}
+get_suexec()(
+    printf '%s' "$SUEXEC" | awk -F: '{print $'$1'}'
+)
 
-if [ "$(id -g)" != 0 ] || [ "$USER_ID" = 0 ]; then
-    export GROUP_ID=${GROUP_ID:-${SUEXEC_GROUP_ID:-$(id -g)}}
-else
-    export GROUP_ID=${GROUP_ID:-${SUEXEC_GROUP_ID:-$(getent group users | awk -F: '{print $3}')}}
-fi
+get_id()(
+    printf '%s' "$ID" | sed -E 's/uid=([0-9]+)(\(([^)]+)\))?[^=]+gid=([0-9]+)(\(([^)]+)\))?.*/\'"$1"'/'
+)
 
-if [ x"$USER_MASK" != x ]; then
-    umask "$USER_MASK"
-fi
-
-if [ "$(id -u)" != 0 ]; then
-    export USER_NAME=${USER_NAME:-$(id -u -n)}
-    export GROUP_NAME=${GROUP_NAME:-$(id -g -n)}
-    return
-fi
-
-if [ "$USER_ID" = 0 ] && [ "$USER_NAME" != "root" ] && [ x"$USER_NAME" != x ]; then
-    printf "User root can not be renamed to %s\n" "$USER_NAME" >&2
-    return
-fi
-
-if [ "$GROUP_ID" = 0 ] && [ "$GROUP_NAME" != "root" ] && [ x"$GROUP_NAME" != x ]; then
-    printf "Group root can not be renamed to %s\n" "$GROUP_NAME" >&2
-    return
-fi
-
-_del_group(){
-    if [ x"$(getent group "$1")" = x ]; then
+del_group()(
+    if [ -z "$1" ] || [ -z "$(getent group "$1")" ]; then
         return
     fi
 
-    for _USER_NAME in $(getent group "$1" | awk -F: '{print $4}' | sed "s/,/ /g"); do
-        deluser "$_USER_NAME" 2> /dev/null
+    group="$(getent group "$1" | cut -d: -s -f1)"
+    for username in $(getent group "$group" | cut -d: -s -f4 | sed "s/,/ /g"); do
+        deluser "$username" 2> /dev/null
     done
 
-    _USER_NAME="$(delgroup "$1" 2>&1 | awk '{print $2}' | tr -d "'")"
-    if [ x"$_USER_NAME" != x ]; then
-        deluser "$_USER_NAME" 2> /dev/null
-        delgroup "$1"
+    username="$(delgroup "$group" 2>&1 | awk '{print $2}' | tr -d "'")"
+    if [ -n "$username" ]; then
+        deluser "$username" 2> /dev/null
+        delgroup "$group"
+    fi
+)
+
+fix_user_group()(
+    exists_group_id="$(getent group "$4" | cut -d: -s -f3)"
+    exists_group_name="$(getent group "$3" | cut -d: -s -f1)"
+    if [ "$exists_group_id" != "$3" ] || [ "$exists_group_name" != "$4" ]; then
+        del_group "$exists_group_name"
+        del_group "$4"
+        addgroup -g "$3" -S "$4"
     fi
 
-    unset -v _USER_NAME
-}
-
-# group name
-_GROUP_NAME=$(getent group "$GROUP_ID" | awk -F: '{print $1}')
-if [ x"$_GROUP_NAME" = x ]; then
-    if [ x"$GROUP_NAME" = x ]; then
-        export GROUP_NAME=users
+    exists_user_id="$(getent passwd "$2" | cut -d: -s -f3)"
+    exists_user_name="$(getent passwd "$1" | cut -d: -s -f1)"
+    if [ -n "$exists_user_name" ] && [ "$exists_user_name" != "$2" ]; then
+        deluser "$exists_user_name"
     fi
 
-    _del_group "$GROUP_NAME"
-    addgroup -g "$GROUP_ID" -S "$GROUP_NAME"
-elif [ x"$GROUP_NAME" = x ]; then
-    export GROUP_NAME="$_GROUP_NAME"
-elif [ x"$_GROUP_NAME" != x"$GROUP_NAME" ]; then
-    _del_group "$_GROUP_NAME"
-    _del_group "$GROUP_NAME"
-    addgroup -g "$GROUP_ID" -S "$GROUP_NAME"
-fi
-
-# user name
-_USER_NAME=$(getent passwd "$USER_ID" | awk -F: '{print $1}')
-if [ x"$_USER_NAME" = x ]; then
-    if [ x"$USER_NAME" = x ]; then
-        export USER_NAME=user
+    if [ -n "$exists_user_id" ] && [ "$exists_user_id" != "$1" ]; then
+        deluser "$2"
     fi
 
-    if [ x"$(getent passwd "$USER_NAME")" != x ]; then
-        deluser "$USER_NAME"
+    exists_group_name="$(groups "$2" 2>/dev/null | awk '{print $1}')"
+    if [ -n "$exists_group_name" ] && [ "$exists_group_name" != "$4" ]; then
+        del_group "$exists_group_name"
     fi
-elif [ x"$USER_NAME" = x ]; then
-    export USER_NAME="$_USER_NAME"
-elif [ x"$_USER_NAME" != x"$USER_NAME" ]; then
-    deluser "$_USER_NAME"
+
+    if [ -z "$(getent passwd "$1")" ]; then
+        adduser -u "$1" -S -s /bin/sh -G "$4" "$2"
+    fi
+
+    echo "$2:password" | chpasswd 2>/dev/null
+)
+
+export USER_ID="$(non_empty "$USER_ID" "$(get_id 1)" "$(get_suexec 1)" "$([ "$(id -u)" != 0 ] && id -u)" "$([ -z "$USER_NO_PWD" ] && stat -c %u $(pwd))" "$(id -u)")"
+export USER_NAME="$(non_empty "$USER_NAME" "$(get_id 3)" "$(get_suexec 3)" "$(getent passwd "$USER_ID" | cut -d: -f1)" "$([ "$(id -u)" != 0 ] && id -u -n 2>dev/null)" "user")"
+export GROUP_ID="$(non_empty "$GROUP_ID" "$(get_id 4)" "$(get_suexec 2)" "$([ "$(id -g)" != 0 ] && id -g)" "$([ -z "$USER_NO_PWD" ] && stat -c %g $(pwd))" "$([ "$USER_ID" != 0 ] && getent group users | cut -d: -s -f3)" "$(id -g)")"
+export GROUP_NAME="$(non_empty "$GROUP_NAME" "$(get_id 6)" "$(getent group "$GROUP_ID" | cut -d: -f1)" "$([ "$(id -u)" != 0 ] && id -g -n 2>dev/null)" "users")"
+export USER_MASK="$(non_empty "$USER_MASK" "$(get_suexec 4)" "$(umask)")"
+
+if [ -n "$USER_MASK" ]; then
+    umask "$USER_MASK"
 fi
 
-_GROUP_NAME=$(groups "$USER_NAME" 2>/dev/null | awk '{print $1}')
-if [ x"$_GROUP_NAME" != x ] && [ x"$_GROUP_NAME" != x"$GROUP_NAME" ]; then
-    _del_group "$_GROUP_NAME"
+if [ "$USER_ID" = 0 ] && [ "$USER_NAME" != 'root' ] && [ -n "$USER_NAME" ]; then
+    printf 'User root can not be renamed to %s\n' "$USER_NAME" >&2
+    export USER_NAME='root'
+    return
 fi
 
-if [ x"$(getent passwd "$USER_ID")" = x ]; then
-    adduser -u "$USER_ID" -D -S -G "$GROUP_NAME" "$USER_NAME"
+if [ "$GROUP_ID" = 0 ] && [ "$GROUP_NAME" != 'root' ] && [ -n "$GROUP_NAME" ]; then
+    printf 'Group root can not be renamed to %s\n' "$GROUP_NAME" >&2
+    export GROUP_NAME='root'
+    return
 fi
+
+if [ "$(id -u)" = 0 ]; then
+    ! fix_user_group "$USER_ID" "$USER_NAME" "$GROUP_ID" "$GROUP_NAME"
+fi
+
+unset -f del_group get_id get_suexec fix_user_group non_empty
