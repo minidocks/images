c5817ea8ad506deadf1d9be8d24e35714a3b0cdc
Martin Hasoň <martin.hason@gmail.com>
2018-11-24 00:46:38 +0100

    [base] Add default dropbear key for ssh

--- base/rootfs/docker-entrypoint.d/20-ssh.sh
+++ base/rootfs/docker-entrypoint.d/20-ssh.sh
@@ -1,44 +1,43 @@
 #!/bin/sh
 #
-# Register SSH key, convert to dropbear format and register known_hosts
+# Register ssh key, convert to dropbear format and register known_hosts
 #
-# (c) 2016 Martin Hasoň <martin.hason@gmail.com>
-#
+# (c) 2016,2018 Martin Hasoň <martin.hason@gmail.com>
 
 set -e
 
-_USER=${USER_NAME:-$(id -u -n)}
-
-_init() {
-    if [ "$_USER" = "$(id -u -n)" ]; then
-        _EXEC=""
-    elif [ "$(id -u)" = 0 ]; then
-        _EXEC="su-exec $_USER"
+su_exec()(
+    user="${USER_NAME:-$(id -u -n)}"
+    if [ "$(id -u)" = 0 ]; then
+        su-exec "$user" "$@"
+    elif [ "$user" = "$(id -u -n)" ]; then
+        "$@"
     else
-        exec printf "\033[30;41mUnable to create SSH directory for user \"%s\" (permission denied)\033[0m\n" "$_USER" >&2
+        exec printf "\\033[30;41mUnable to create SSH directory for user \"%s\" (permission denied)\\033[0m\\n" "$user" >&2
     fi
+)
 
-    _HOME=$($_EXEC sh -c 'echo $HOME')
-    $_EXEC mkdir -p "$_HOME"/.ssh
-}
+get_home()(
+    home="$(su_exec sh -c 'echo $HOME')"
+    su_exec mkdir -p "$home/.ssh"
+    printf '%s' "$home"
+)
 
-if [ x"$SSH_ID_DROPBEAR" != x ]; then
-    _init
-    $_EXEC touch "$_HOME/.ssh/id_dropbear"
-    echo "$SSH_ID_DROPBEAR" | tr -d '\r' > "$_HOME/.ssh/id_dropbear"
-elif [ x"$SSH_ID_RSA" != x ]; then
-    _init
-    $_EXEC touch "$_HOME/.ssh/id_rsa"
-    echo "$SSH_ID_RSA" | tr -d '\r' > "$_HOME/.ssh/id_rsa"
-    chmod 600 "$_HOME/.ssh/id_rsa"
-    $_EXEC dropbearconvert openssh dropbear "$_HOME/.ssh/id_rsa" "$_HOME/.ssh/id_dropbear"
+if [ -n "$SSH_ID_DROPBEAR" ]; then
+    su_exec touch "$(get_home)/.ssh/id_dropbear"
+    printf '%s\n' "$SSH_ID_DROPBEAR" | tr -d '\r' > "$(get_home)/.ssh/id_dropbear"
 fi
 
+if [ -n "$SSH_ID_RSA" ]; then
+    su_exec touch "$(get_home)/.ssh/id_rsa"
+    printf '%s\n' "$SSH_ID_RSA" | tr -d '\r' > "$(get_home)/.ssh/id_rsa"
+    chmod 600 "$(get_home)/.ssh/id_rsa"
+    ! su_exec dropbearconvert openssh dropbear "$(get_home)/.ssh/id_rsa" "$(get_home)/.ssh/id_dropbear" 2>/dev/null
+fi
+
+su_exec touch "$(get_home)/.ssh/known_hosts"
 if [ -r "/etc/dropbear/ssh_known_hosts" ]; then
-    _init
-    $_EXEC touch "$_HOME/.ssh/known_hosts"
-    cat "/etc/dropbear/ssh_known_hosts" >> "$_HOME/.ssh/known_hosts"
+    cat "/etc/dropbear/ssh_known_hosts" >> "$(get_home)/.ssh/known_hosts"
 fi
 
-unset -v _USER _HOME _EXEC
-unset -f _init
+unset -f su_exec get_home
