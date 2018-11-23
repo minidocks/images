cfadf0f2192f75f3d38fe8ea6b6ca9c39638f78d
Martin Hasoň <martin.hason@gmail.com>
2018-11-24 00:50:45 +0100

    [base] Fix named pipes

--- base/rootfs/docker-entrypoint.d/05-first-process-stream.sh
+++ base/rootfs/docker-entrypoint.d/05-first-process-stream.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# Create named pipes for the first process
+# Create named pipes for the stdout and stderr
 #
 # (c) 2018 Martin Hasoň <martin.hason@gmail.com>
 #
 # https://en.wikipedia.org/wiki/Named_pipe
 # https://github.com/moby/moby/issues/19616#issuecomment-174355979
-#
+# https://stackoverflow.com/questions/8410439/how-to-avoid-echo-closing-fifo-named-pipes-funny-behavior-of-unix-fifos
 
 set -e
 
@@ -15,6 +15,6 @@ if [ -w /dev ]; then
     mkfifo /dev/stderr.pipe
     chmod a+rw /dev/stdout.pipe /dev/stderr.pipe
 
-    cat /dev/stdout.pipe > /proc/1/fd/1 &
-    cat /dev/stderr.pipe > /proc/1/fd/2 &
+    cat <>/dev/stdout.pipe > /proc/1/fd/1 &
+    cat <>/dev/stderr.pipe > /proc/1/fd/2 &
 fi
