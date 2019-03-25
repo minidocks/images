7a6acca82667e3eab7493b9372bf3ccf0f6d2088
Martin Hasoň <martin.hason@gmail.com>
2019-03-25 23:18:01 +0100

    [base] Use /bin directory for aliases

--- base/rootfs/docker-entrypoint.d/10-alias.sh
+++ base/rootfs/docker-entrypoint.d/10-alias.sh
@@ -12,19 +12,19 @@ register_aliases()(
     for name in $(printenv | grep -E "^ALIAS_" | cut -d= -f1); do
         alias="$(echo "${name#*_}" | tr '[:upper:]' '[:lower:]')"
 
-        if [ -f "/usr/local/bin/$alias" ]; then
-            printf "\033[30;41mCommand \"/usr/local/bin/%s\" for environment variable \"%s\" already exists\033[0m\n" "$alias" "$name" >&2
+        if [ -f "/bin/$alias" ]; then
+            printf "\033[30;41mCommand \"/bin/%s\" for environment variable \"%s\" already exists\033[0m\n" "$alias" "$name" >&2
             continue
         fi
 
-        if [ ! -w /usr/local/bin ]; then
-            printf "\033[30;41mUnable to create file \"/usr/local/bin/%s\" for environment variable \"%s\" (permission denied)\033[0m\n" "$alias" "$name" >&2
+        if [ ! -w /bin ]; then
+            printf "\033[30;41mUnable to create file \"/bin/%s\" for environment variable \"%s\" (permission denied)\033[0m\n" "$alias" "$name" >&2
             continue
         fi
 
-        value="$(eval printf '%s' \"\$${name}\")"
-        printf '#!/bin/sh\nset -e\n %s "$@"' "$value" >"/usr/local/bin/$alias"
-        chmod a+x "/usr/local/bin/$alias"
+        value="$(eval printf '%s' \"\$\{${name}\}\")"
+        printf '#!/bin/sh\nset -e\n%s "$@"' "$value" >"/bin/$alias"
+        chmod a+x "/bin/$alias"
     done
 )
 
