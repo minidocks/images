d2884400294eb77f52ab202a1f3b6ffc9124619c
Martin Hasoň <martin.hason@gmail.com>
2021-04-06 23:58:40 +0200

    Fix version

--- sphinx-doc/build.sh
+++ sphinx-doc/build.sh
@@ -3,10 +3,10 @@ set -e
 
 image="${namespace:-minidocks}/sphinx-doc"
 versions="
-3;3.5.1
-3-pdf;3.5.1;weasyprint;pdf
-pdf;3.5.1;weasyprint;pdf
-latest;3.5.1
+3;3.5.3
+3-pdf;3.5.3;weasyprint;pdf
+pdf;3.5.3;weasyprint;pdf
+latest;3.5.3
 "
 
 build() {
