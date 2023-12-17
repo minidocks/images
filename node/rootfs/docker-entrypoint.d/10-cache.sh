be7333339f90fd2f9d63d248d6341bdb5bf16098
Martin Hasoň <martin.hason@gmail.com>
2023-12-17 01:25:56 +0100

    Fix creating cache dirs

--- node/rootfs/docker-entrypoint.d/10-cache.sh
+++ node/rootfs/docker-entrypoint.d/10-cache.sh
@@ -2,7 +2,7 @@
 set -e
 
 for dir in "$NPM_CONFIG_CACHE" "$YARN_CACHE_FOLDER" $(echo "$NODE_PATH" | tr ":" "\n"); do
-  if [ -n "$dir" ]; then
+  if [ -n "$dir" ] && [ ! -d "$dir" ]; then
     mkdir -p "$dir"
     chmod -R a+rwX "$dir"
   fi
